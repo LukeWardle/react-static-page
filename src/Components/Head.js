cb1b264a0bc89bcbd13b4ad9bb6c7dd8c50b0c9b
lward <lward@LUKE>
2023-05-14 08:26:24 +0100

    project finished

---
+++ src/Components/Head.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+
+function Head () {
+  return (
+      <nav>
+        <img src={logo} alt="React logo" />
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
+      </nav>
+  );
+}
+
+export default Head;
